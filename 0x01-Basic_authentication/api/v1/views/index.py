68f3c449c1b5956f7511a9617e2a2f8fd1a763ac
bg-miles <piuschukwuebuka40@gmail>
2024-09-06 14:26:47 +0100

    setup

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -27,7 +27,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route("/unauthorized", methods=["GET"], strict_slashes=False)
+@app_views.route("/unauthorized/", methods=["GET"], strict_slashes=False)
 def unauthorized() -> None:
     """GET /api/vi/unathorized
     Return:
@@ -36,7 +36,7 @@ def unauthorized() -> None:
     abort(401, description="Unauthorized")
 
 
-@app_views.route("/forbidden", methods=["GET"], strict_slashes=False)
+@app_views.route("/forbidden/", methods=["GET"], strict_slashes=False)
 def forbidden() -> None:
     """GET /api/vi/forbidden
     Return:
