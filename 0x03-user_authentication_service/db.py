a50d125554b96655da102b3735a0d56f85bece8e
bg-miles <piuschukwuebuka40@gmail>
2024-09-13 00:05:39 +0100

    auth

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """DB module
 """
 
@@ -50,8 +51,7 @@ class DB:
     def update_user(self, user_id, **kwargs):
         """Finds and updates a user"""
         user = self.find_user_by(id=user_id)
-        new_user = self._session.query(User).filter(User.id == user.id)\
-            .update(kwargs)
+        new_user = self._session.query(User).filter(User.id == user.id).update(kwargs)
         if new_user:
             session = self._session
             session.commit()
