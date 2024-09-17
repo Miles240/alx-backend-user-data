7c63dce476b75728659926aaa65169226e12743d
bg-miles <piuschukwuebuka40@gmail>
2024-09-17 15:40:59 +0100

    add some doc strings

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -18,7 +18,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance"""
         self._engine = create_engine("sqlite:///a.db", echo=False)
-        Base.metadata.drop_all(self._engine)
+        Base.metadata.drop_all(self._engine)  # You don't wanna do this in real life 😂
         Base.metadata.create_all(self._engine)
         self.__session = None
 
@@ -31,7 +31,14 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> None:
-        """Adds a new user to the database"""
+        """
+        Adds a new user to the database
+            Args:
+                email (str): user's email
+                hashed_password (str): user's password, a string for now
+            Returns:
+                A newly created user object
+        """
         new_user = User(email=email, hashed_password=hashed_password)
         session = self._session
         session.add(new_user)
@@ -43,20 +50,27 @@ class DB:
         """
         Retrives a user from the database
             Args:
-                kwargs(dict): a dictionary of attributes
+                kwargs (dict): a dictionary of user's details
             Returns:
-                user(dict): the first user in the database
+                user (dict): the first user in the database
         """
         try:
             user = self._session.query(User).filter_by(**kwargs).first()
             if user is None:
                 raise NoResultFound
             return user
-        except InvalidRequestError as invalid:
-            raise invalid
+        except InvalidRequestError:
+            return None
 
     def update_user(self, user_id: int, **kwargs) -> None:
-        """Finds and updates a user"""
+        """
+        Finds and updates a user
+            Args:
+                user_id (int): id of the user to be updated
+                kwargs (dict): user full details
+            Returns:
+                None
+        """
         user = self.find_user_by(id=user_id)
         new_user = self._session.query(User).filter(User.id == user.id).update(kwargs)
         session = self._session
